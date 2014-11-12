266068d2f37047b7b5fda259ce25b9e196e0ee89
vprefournier <v.prefournier@gmail.com>
2014-11-12 16:31:41 -0500

    add search bar

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  console.log("HALLO!");
-  res.sendfile('public/index.html');
+router.get('/', function (req, res) {
+    res.render('index', {
+        title: 'Home'
+    });
 });
 
 module.exports = router;
